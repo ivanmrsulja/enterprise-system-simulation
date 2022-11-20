91ff30cb851fac1645278c42f3d6baeada84e72c
milospp <misa01523@gmail.com>
2022-11-20 19:59:06 +0100

    Added 3 front and card form

---
+++ acquirer-bank-front/src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("../views/CreditCardPayment.vue"),
+    },
+    {
+      path: "/card-payment",
+      name: "card-payment",
+      component: () => import("../views/CreditCardPayment.vue"),
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue"),
+    },
+  ],
+});
+
+export default router;
