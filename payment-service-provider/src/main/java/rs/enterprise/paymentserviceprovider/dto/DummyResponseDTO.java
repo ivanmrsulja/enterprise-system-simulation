5c915ac499dd62aec2c3f78bf8f7e7c3e55e1187
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2022-12-27 15:52:05 +0100

    Added some mock payments. Oh Yeah mock it!

---
+++ payment-service-provider/src/main/java/rs/enterprise/paymentserviceprovider/dto/DummyResponseDTO.java
@@ -0,0 +1,14 @@
+package rs.enterprise.paymentserviceprovider.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DummyResponseDTO {
+    private String result;
+}
