fda42df7bb8acc6039ced2d9e7cb3c87954e19a5
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2023-01-31 17:48:46 +0100

    Added test accounts for issuer bank.

--- issuer-bank/util/databaseDriver.go
+++ issuer-bank/util/databaseDriver.go
@@ -12,7 +12,7 @@ var (
 	accounts = []model.BankAccount{
 		{Pan: Bytes2StrRaw(Hash("1231231231")), SecurityCode: Bytes2StrRaw(Hash("123")), CardHolderName: Bytes2StrRaw(Hash("Ivan Mrsulja")), ExpiryDate: Bytes2StrRaw(Hash("11/24")), Balance: 10000},
 		{Pan: Bytes2StrRaw(Hash("1231213213")), SecurityCode: Bytes2StrRaw(Hash("321")), CardHolderName: Bytes2StrRaw(Hash("Milos Popovic")), ExpiryDate: Bytes2StrRaw(Hash("05/23")), Balance: 20000},
-		{Pan: Bytes2StrRaw(Hash("1231211111")), SecurityCode: Bytes2StrRaw(Hash("456")), CardHolderName: Bytes2StrRaw(Hash("Veljko Tosic")), ExpiryDate: Bytes2StrRaw(Hash("08/25")), Balance: 30000},
+		{Pan: Bytes2StrRaw(Hash("1231211111")), SecurityCode: Bytes2StrRaw(Hash("456")), CardHolderName: Bytes2StrRaw(Hash("Veljko Tosic")), ExpiryDate: Bytes2StrRaw(Hash("08/25")), Balance: 300},
 	}
 )
 
